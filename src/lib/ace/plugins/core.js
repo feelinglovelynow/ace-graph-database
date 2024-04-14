85500e8767d09caee754eed22edfd80c960b1779
Chris Carrington <carrington.christopher@gmail.com>
2024-04-14 15:05:19 -0700

    Update core.js

--- src/lib/ace/plugins/core.js
+++ src/lib/ace/plugins/core.js
@@ -13,7 +13,7 @@ export function core () {
         {
           id: 'SchemaAdd',
           x: {
-            // allowAcePrefix: true,
+            allowAcePrefix: true,
             schema: {
               nodes: {
                 AceSetting: {
